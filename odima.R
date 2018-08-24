4402fe2c6cfd0e7d14a2aa2aaec8d0f5eb88b894
Mark H <28765863+Mark1plus1@users.noreply.github.com>
2018-08-24 12:28:17 -0400

    Update odima.R

--- odima.R
+++ odima.R
@@ -13,24 +13,32 @@ permuteDist = function(d){
   as.dist(d[p,p])
 }
 
-pdCorMediationTest = function(exposure, mediator, response, nrep){
-  if (is.null(nrep))
+pdCorMediationTest = function(exposure, mediator, response, nrep=NULL){
+  method <- "WARNING:Specify the number of replicates nrep>0 to perform the test"
+  if (!is.null(nrep)){
+    nrep <- floor(nrep)
+    if (nrep <1)
+      nrep <- 0
+    if (nrep > 0)
+      method <- "pdCor mediation test of independence of mediator and response removing exposure"
+  }
+  else {
     nrep <- 0
+  }
   if(bcdcor(exposure, mediator)< pdcor(mediator, response, exposure)){
     S <- replicate(nrep,expr = bcdcor(permuteDist(exposure),mediator)*pdcor(mediator, response, exposure))
   }
   else{
     S <- replicate(nrep, expr = bcdcor(exposure,mediator)*pdcor(mediator, permuteDist(response), exposure))
   }
-  mediationTest <- (1+sum(S>mediationStat(exposure, mediator, response)))/(nrep+1)
+  p.value <- ifelse( nrep>0, ((1+sum(S>mediationStat(exposure, mediator, response)))/(nrep+1)), 1)
   dataname <- paste("replicates ", nrep, sep = "")
-  method <- ifelse(nrep > 0, "pdCor mediation test of independence of mediator and response removing exposure", "WARNING:Specify the number of replicates nrep>0 to perform the test")
   bcdcor_estimates <- c(bcdcor(exposure, mediator)[1], bcdcor(exposure, response), bcdcor(mediator, response)#, pdcor(distjsd_early, dR_early, dE_early)
                         )
     e <- list(method = method, 
             statistic = mediationStat(exposure, mediator, response), 
             estimates = bcdcor_estimates,
-            p.value = mediationTest,
+            p.value = p.value,
             replicates = nrep,
             data.name = dataname)
   class(e) <- "htest"
